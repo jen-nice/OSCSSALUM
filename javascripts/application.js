f86648aeae6755c9c0aa2218ea5fdaa8cbf9ed3d
jencol <jennice.colaco@gmail.com>
2014-02-26 01:20:59 -0500

    Update application.js

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,21 @@
 $(document).ready(function (){
-$("h1").on("click", function(){
+  $("#slideshow").css("overflow", "hidden");
+  $("ul#slides").cycle({
+	fx: 'fade',
+	pause: 1,
+	prev: '#prev',
+	next: '#next'
+});
+
+$("#slideshow").hover(function() {
+	$("ul#nav").fadeIn();
+	},
+		function() {
+	$("ul#nav").fadeOut();
+	});
+/*$("h1").on("click", function(){
   var message = $("<span>Hbibti</span>");
-  $("h1").text('message');
+  $("h1").text('message');*/
 });
 });
 
