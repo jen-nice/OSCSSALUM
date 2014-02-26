4f71693f9e150094b8723e20d8bf86f3360c1f1a
jencol <jennice.colaco@gmail.com>
2014-02-25 23:21:35 -0500

    Update application.js

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,6 @@
 $(document).ready(function (){
-$("#java").on("click", function(){
-  var message = $("<span>Hubba</span>");
-  $("h1").append(message);
-  $("#java").remove();
+$("h1").on("click", function(){
+  alert("Home Boys");
 });
 });
+
